23419a75314394c14cb6256dafbe8ba4c3b9fedf
pccode <arvindj97@gmail.com>
2021-07-02 20:07:24 +0530

    UseEffect Variation 3

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import Us from './UseState/Us';
 import Ue1 from './UseEffect/Ue1';
 import Ue2 from './UseEffect/Ue2';
+import Ue3 from './UseEffect/Ue3';
 function App() {
   return (
-<>
-{/* <h1>Heelo</h1> */}
-{/* <Us/> */}
-{/* <Ue1/> */}
-<Ue2/>
-</>
+    <>
+      {/* <h1>Heelo</h1> */}
+      {/* <Us/> */}
+      {/* <Ue1/> */}
+      {/* <Ue2/> */}
+      {/* <Infinite/> */}
+      <Ue3 />
+    </>
   );
 }
 
