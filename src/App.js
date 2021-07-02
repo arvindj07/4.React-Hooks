9a40e454aa4bd523cdb16c7e149bb1464ed370f5
pccode <arvindj97@gmail.com>
2021-07-02 19:27:07 +0530

    UseEffect variation 1 and 2

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import Us from './UseState/Us';
+import Ue1 from './UseEffect/Ue1';
+import Ue2 from './UseEffect/Ue2';
+function App() {
+  return (
+<>
+{/* <h1>Heelo</h1> */}
+{/* <Us/> */}
+{/* <Ue1/> */}
+<Ue2/>
+</>
+  );
+}
+
+export default App;
